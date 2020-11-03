74a4d0b557da943abaaa10f95306ec0411c6e660
Carlos Hernandez <cdhernandezp@gmail.com>
2020-11-03 16:40:39 -0500

    Fix Middleware & version

--- src/CL7Logger/Extensions/LogManagerBuilderHelper.cs
+++ src/CL7Logger/Extensions/LogManagerBuilderHelper.cs
@@ -25,28 +25,6 @@ namespace CL7Logger.Extensions
         {
             app.UseMiddleware<CL7LogMiddleware>();
 
-            app.Use(async (ctx, next) =>
-            {
-                ConnectionStringManager connectionStringManager = ctx.RequestServices.GetService<ConnectionStringManager>();
-                IOptions<CL7LogOptions> options = ctx.RequestServices.GetService<IOptions<CL7LogOptions>>();
-
-                //If its the first time that you use a connectionString then verify if has the tables created!
-                if (!connectionStringManager.ConnectionAttempts.Contains(options.Value.ConnectionString))
-                {
-                    await DatabaseManager.CreateTableIfNotExists(options.Value.ConnectionString);
-                    connectionStringManager.ConnectionAttempts.Add(options.Value.ConnectionString);
-                }
-
-                //Try to get TraceId value from HttpRequest Header
-                StringValues traceValues;
-                if (ctx.Request.Headers.TryGetValue(options.Value.TraceIdHeaderName, out traceValues) && traceValues.Count > 0)
-                    options.Value.TraceId = Guid.Parse(traceValues[0]);
-                else
-                    options.Value.TraceId = Guid.NewGuid();
-
-                await next();
-            });
-
             return app;
         }
     }
